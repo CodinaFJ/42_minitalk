425f10f0f7781bdb91073b6bfee4db4642e05fe2
jcodina- <fjavier.codina@gmail.com>
2023-10-24 19:54:59 +0200

    Add 42 headers

--- src/server/server.c
+++ src/server/server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcodina- <jcodina-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 19:54:43 by jcodina-          #+#    #+#             */
+/*   Updated: 2023/10/24 19:54:44 by jcodina-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -7,8 +19,13 @@
 
 // Función de manejo de señales
 void signal_handler(int signal) {
-    printf("Se recibió la señal %d (Ctrl+C).\n", signal);
+    ft_printf("Se recibió la señal %d (Ctrl+C).\n", signal);
     exit(1);
+}
+
+void    client_signal_handler(int signum, siginfo_t* info, void* context)
+{
+    ft_printf("Client contacted\n");
 }
 
 int main()
